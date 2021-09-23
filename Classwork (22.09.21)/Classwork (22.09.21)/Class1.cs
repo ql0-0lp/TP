2a833f34a37c9f5096a2fe580e1a7759eae4c765
ql0-0lp <mail@4e1.ru>
2021-09-23 21:06:25 +0300

    Add all

---
+++ Classwork (22.09.21)/Classwork (22.09.21)/Class1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classwork__22._09._21_
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            //Cw.One();
+            //Cw.Two();
+            //Cw.Three();
+            //Cw.Four();
+            Cw.Five();
+        }
+    }
+
+    class Cw
+    {
+        public static void One()
+        {
+            Console.WriteLine("Введите ваш пол");
+            string s = Console.ReadLine();
+            switch (s)
+            {
+                case "М":
+                    Console.WriteLine("Введите ваш рост");
+                    double l = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine((l  - 100) * 1.15);
+                    break;
+                case "Ж":
+                    Console.WriteLine("Введите ваш рост");
+                    l = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine((l - 110) * 1.15);
+                    break;
+                case "м":
+                    Console.WriteLine("Введите ваш рост");
+                    l = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine((l - 100) * 1.15);
+                    break;
+                case "ж":
+                    Console.WriteLine("Введите ваш рост");
+                    l = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine((l - 110) * 1.15);
+                    break;
+                default:
+                    break;
+            }
+            Console.ReadLine();
+        }
+
+
+
+         public static void Two()
+        {
+            Console.WriteLine("Введите колличество цифр в числе");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[b];
+            Console.WriteLine($"Введите {b} цфир(-ы)");
+            for (int i = 0; i < b; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = b-1; i > -1; i--)
+            {
+                Console.Write(array[i]);
+            }
+            Console.ReadLine();
+
+        }
+
+
+
+        public static void Three()
+        {
+            bool k = false;
+            Console.WriteLine("Введите колличество цифр в массиве");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[b];
+            Console.WriteLine($"Введите {b} цфир(-ы)");
+            for (int i = 0; i < b; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < b; i++)
+            {
+                if ((array[i] == 1) && (array[i+1] == 2) && (array[i + 2] == 3))
+                {
+                    k = true;
+                }
+            }
+            if (k == true)
+            {
+                Console.WriteLine("Да, есть последовательность 123");
+            }
+            else
+            {
+                Console.WriteLine("Нет последовательности 123");
+            }
+            Console.ReadLine();
+        }
+
+
+
+        public static void Four()
+        {
+            bool m = false;
+            bool n = false;
+            int k = 0;
+            int k2 = 0;
+            Console.WriteLine("Введите колличество чисел в массиве");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[b];
+            Console.WriteLine($"Введите {b} числа (чисел)");
+            for (int i = 0; i < b; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < b; i++)
+            {
+                int v = array[i];
+                if (v != 6)
+                {
+                    k = v + k;
+                    if ((v == 7) && (m == true))
+                    {
+                        k -= k2;
+                        k -= 7;
+
+                        m = false;
+                        n = false;
+                    }
+                    if (n == true)
+                    {
+                        k2 += v;
+                    }
+                }
+                else
+                {
+                    k += v;
+                    k2 += v;
+                    m = true;
+                    n = true;
+                }
+            }
+            Console.WriteLine(k);
+            Console.ReadLine();
+        }
+
+
+        public static void Five()
+        {
+
+        }
+        
+    }
+}
---
+++ Homework (21.09.21)/Homework (21.09.21)/Class1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework__21._09._21_
+{
+    class Run
+    {
+        static void Main(string[] args)
+        {
+            Class1.One();
+            //Class1.Two();
+            //Class1.Three();
+            //Class1.Four();
+            //Class1.Five();
+            //Class1.Six();
+        }
+    }
+
+    class Class1
+    {
+
+        public static void One()
+        {
+            Console.WriteLine("Введите скидку:");
+            decimal x = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Введите количество денег, которые вы готовы потратить:");
+            decimal n = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(n - (n * (x / 100)));
+            Console.ReadLine();
+        }
+
+
+
+        public static void Two()
+        {
+            Console.WriteLine("Сколько мест в театре?");
+            decimal y = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Сколько было продано билетов?");
+            decimal x = Int32.Parse(Console.ReadLine());
+            Console.WriteLine($"{(x / y) * 100}%");
+            Console.ReadLine();
+        }
+
+
+
+        public static void Three()
+        {
+            for (int z = 1; z < 10; z++)
+            {
+                for (int t = 0; t < 10; t++)
+                {
+                    for (int y = 0; y < 10; y++)
+                    {
+                        for (int r = 1; r < 10; r++)
+                        {
+                            for (int s = 0; s < 10; s++)
+                            {
+                                if ((z * 100) + (t * 10) + y + (r * 1000) + (z * 100) + (y * 10) + r == (s * 1000) + (z * 100) + (r * 10) + z)
+                                {
+                                    Console.WriteLine($"{z}{t}{y} + {r}{z}{y}{z} = {s}{z}{r}{z}");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+
+
+
+        public static void Four()
+        {
+            for (int i = 1000; i < 10000; i++)
+            {
+                if (((i / 1000) != (i % 1000 / 100)) && ((i % 1000 % 100 / 10) != (i % 1000 % 100 % 10)) && ((i / 1000) != (i % 1000 % 100 / 10)) && ((i % 1000 / 100) != (i % 1000 % 100 % 10)) && ((i % 1000 / 100) != (i % 1000 % 100 / 10)) && ((i / 1000) != (i % 1000 % 100 % 10)))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+        }
+
+
+
+        public static void Five()
+        {
+            Console.WriteLine("Введите четырёхзначное число");
+            int i = Int32.Parse(Console.ReadLine());
+            if (((i / 1000) == (i % 1000 / 100)) || ((i % 1000 % 100 / 10) == (i % 1000 % 100 % 10)) || ((i / 1000) == (i % 1000 % 100 / 10)) || ((i % 1000 / 100) == (i % 1000 % 100 % 10)) || ((i % 1000 / 100) == (i % 1000 % 100 / 10)) || ((i / 1000) == (i % 1000 % 100 % 10)))
+            {
+                Console.WriteLine("Да, есть повторяющиеся цифры");
+            }
+            else
+            {
+                Console.WriteLine("Нет повторяющихся цифр");
+            }
+            Console.ReadLine();
+        }
+
+
+
+        public static void Six(string[] args)
+        {
+            for (int i = 20; i < 51; i++)
+            {
+                if ((i % 3 == 0) && (i % 5 != 0))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+
+}
